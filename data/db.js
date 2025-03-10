092cdca78e9eda89be9acacf2e1b867ea9d669ef
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-10 16:18:45 +0100

    fix db.js

--- data/db.js
+++ data/db.js
@@ -1,20 +1,21 @@
-//importo il modulo mysql per connettermi al database MySQL
-const mysql = require('mysql2');
+//importo il modulo mysql2 per la connessione al db MySQl
+const mysql = require("mysql2");
 
 //creo la connessione al db con le variabili di ambiente
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "PassWord8!",
+    database: process.env.DB_NAME || "movies_db",
 });
 
 //effettuo la connessione al db
 connection.connect((err) => {
+    //gestione errore
     if (err) throw err;
-    //messaggio in caso di connessione avvenuta con successo
+    //messaggio in caso di connessione avvenuta con suggesso
     console.error('Connected to MySQL!');
 });
 
-//esporto la connessione
+//esporto la connessione perché possa essere usata in altri file
 module.exports = connection;
