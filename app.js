6b65742795ae5bc8ea9e0874063a244c316f66a0
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-10 16:26:34 +0100

    fix app.js

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const moviesRoutes = require("./routers/movies");
-const db = require("./data/db");
-require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-app.use(express.json());
 
-// Rotte
-app.use("/api/movies", moviesRoutes);
-
-// Avvia il server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`✅ Server in ascolto su http://localhost:${PORT}`);
+// avvio del server sulla porta specificata
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
 });
