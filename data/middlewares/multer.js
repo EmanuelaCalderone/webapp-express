c9e46507d2b21e2ad7b660bfe183463edee2a26c
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-03-14 16:22:46 +0100

    install multer + create middleware multer

---
+++ data/middlewares/multer.js
@@ -0,0 +1,22 @@
+//importo il middleware multer per gestire l'upload dei file
+
+const multer = require("multer");
+
+//configuro il middleware per il salvataggio dei file
+const storage = multer.diskStorage({
+
+    destination: "./public/img/movies/",
+
+    filename: (req, file, cb) => {
+        //genero un nome univoco per ogni file e uso data attuale per non sovrascrivere file con nome uguale
+        const uniqueName = `${Date.now()}-${file.originalname}`;
+
+        //passo il nome del file alla callback per il salvataggio
+        cb(null, uniqueName);
+    }
+});
+
+//creo un'istanza di multer grazie alla configurazione definita prima
+const upload = multer({ storage });
+
+module.exports = upload;
